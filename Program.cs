3dcef4a0685bf9cdb0c1230df8f3d96bd89b28f0
zanlaniakea <robi.zanpaktou@gmail.com>
2020-07-05 09:47:42 +0300

    it semi works

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.IO;
 
 namespace ShopInConsole
@@ -9,25 +10,22 @@ namespace ShopInConsole
         {
 
 
-            int chooseOption = 1;
-
+            int chooseOption =  5;
             do
             {
-
-                var chooseOption1 = chooseOption;
-
+                
                 Console.WriteLine("#1. Enter Shop");
                 Console.WriteLine("#2. Enter AdminPanel");
                 Console.WriteLine("#0. Exit");
                 Console.WriteLine("ChooseOption: ");
-                chooseOption1 = Convert.ToInt32(Console.ReadLine());
-
+                var chooseOption1 = Convert.ToInt32(Console.ReadLine());
+               
                 do
                 {
 
-
                     if (chooseOption1 == 1)
                     {
+
                         Console.WriteLine("Welcome to the shop area.");
                         Console.WriteLine("");
                         Console.WriteLine("1. View Products");
@@ -35,36 +33,65 @@ namespace ShopInConsole
                         Console.WriteLine("3. Add To Cart");
                         Console.WriteLine("0. To Go Back");
                         Console.WriteLine("ChooseOption: ");
-                        chooseOption1 = Convert.ToInt32(Console.ReadLine());
-                        while (chooseOption1 != 0)
+                        var chooseOption2 = Convert.ToInt32(Console.ReadLine());
+
+                        do { 
+                        if (chooseOption2 == 1)
                         {
-                            if (chooseOption1 == 1)
+                            Console.WriteLine("You have arived at View Product");
+                            Console.WriteLine("");
+                            Console.WriteLine("Choose 0 to go back");
+                            var back = Convert.ToInt32(Console.ReadLine());
+                            if (back == 0)
                             {
-                                Console.WriteLine("You have arived at View Product");
+                                Console.WriteLine("");
+                                    chooseOption2 = back;
+                                }
 
-                            }
-                            else if (chooseOption1 == 2)
+                        }
+                        else if (chooseOption2 == 2)
+                        {
+                            Console.WriteLine("You have arived at Add Product");
+                            Console.WriteLine("");
+                            Console.WriteLine("Choose 0 to go back");
+                            var back = Convert.ToInt32(Console.ReadLine());
+                            if (back == 0)
                             {
-                                Console.WriteLine("You have arived at Add Product");
+                                Console.WriteLine("");
+                                    chooseOption2 = back;
+                                }
 
-                            }
-                            else if (chooseOption1 == 3)
+                        }
+                        else if (chooseOption2 == 3)
+                        {
+                            Console.WriteLine("You have arrived to Edit Product");
+                            Console.WriteLine("");
+                            Console.WriteLine("Choose 0 to go back");
+                            var back = Convert.ToInt32(Console.ReadLine());
+                            if (back == 0)
                             {
-                                Console.WriteLine("You have arrived to Edit Product");
+                                Console.WriteLine("");
+                                    chooseOption2 = back;
+                                }
 
-                            }
-                            else
-                            {
-                                Console.WriteLine("You have entered an invalid option.");
+                        }
+                        else
+                        {
 
-                            }
+                            Console.WriteLine("");
+                            chooseOption2 = 0;
+                            chooseOption1 = chooseOption2;
+
                         }
 
-
+                        } while (chooseOption2 != 0);
                     }
+                       
+
 
                     else if (chooseOption1 == 2)
                     {
+
                         Console.WriteLine("Welcome to the admins area.");
                         Console.WriteLine("");
                         Console.WriteLine("1. View Product");
@@ -73,60 +100,80 @@ namespace ShopInConsole
                         Console.WriteLine("4. Delete Product");
                         Console.WriteLine("0. To Go Back");
                         Console.WriteLine("ChooseOption: ");
-                        chooseOption1 = Convert.ToInt32(Console.ReadLine());
+                        var chooseOption2 = Convert.ToInt32(Console.ReadLine());
 
-                        while (chooseOption1 != 0)
+                        do { 
+                        if (chooseOption2 == 1)
                         {
-                            if (chooseOption1 == 1)
+                            Console.WriteLine("You have arived at View Product");
+                            Console.WriteLine("");
+                            Console.WriteLine("Choose 0 to go back");
+                            var back = Convert.ToInt32(Console.ReadLine());
+                            if (back == 0)
                             {
-                                Console.WriteLine("You have arived at View Product");
+                                Console.WriteLine("");
+                                    chooseOption2 = back;
+                                }
 
-                            }
-                            else if (chooseOption1 == 2)
+                        }
+
+                        else if (chooseOption2 == 2)
+                        {
+                            Console.WriteLine("You have arived at Add Product");
+                            Console.WriteLine("");
+                            Console.WriteLine("Choose 0 to go back");
+                            var back = Convert.ToInt32(Console.ReadLine());
+                            if (back == 0)
                             {
-                                Console.WriteLine("You have arived at Add Product");
+                                Console.WriteLine("");
+                                    chooseOption2 = back;
+                                }
+                        }
 
-                            }
-                            else if (chooseOption1 == 3)
+                        else if (chooseOption2 == 3)
+                        {
+                            Console.WriteLine("You have arrived to Edit Product");
+                            Console.WriteLine("");
+                            Console.WriteLine("Choose 0 to go back");
+                            var back = Convert.ToInt32(Console.ReadLine());
+                            if (back == 0)
                             {
-                                Console.WriteLine("You have arrived to Edit Product");
+                                Console.WriteLine("");
+                                    chooseOption2 = back;
+                                }
+                        }
 
-                            }
-                            else if (chooseOption1 == 4)
+                        else if (chooseOption2 == 4)
+                        {
+                            Console.WriteLine("You have arrived to Delete Product");
+                            Console.WriteLine("");
+                            Console.WriteLine("Choose 0 to go back");
+                            var back = Convert.ToInt32(Console.ReadLine());
+                            if (back == 0)
                             {
-                                Console.WriteLine("You have arrived to Delete Product");
-                            }
-
-                            else
-                            {
-                                Console.WriteLine("You have entered an invalid option.");
-
+                                Console.WriteLine("");
+                                chooseOption2 = back;
                             }
                         }
 
+                            else
+                            {
+                                Console.WriteLine("");
+                                chooseOption2 = 0;
+                                chooseOption1 = chooseOption2;
+                            }
 
+                        } while (chooseOption2 != 0);
                     }
-                    else if (chooseOption1 == 0) 
-                    {
-                        chooseOption = 0;
-                    }
-
 
                     else
                     {
-                        Console.WriteLine("You have entered an invalid option.");
+                        chooseOption1 = 0;
+                        chooseOption = chooseOption1;
                     }
 
-
-
-
                 } while (chooseOption1 != 0);
-
-            if (chooseOption == 0)
-                {
-                    break;
-                }
-
+                
             } while (chooseOption != 0);
 
 
